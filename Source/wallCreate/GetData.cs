90dcd26608f647232eef75bd66f23887f64c2f4e
ashishkamblecctech <149309502+ashishkamblecctech@users.noreply.github.com>
2024-04-24 10:59:13 +0530

    Add files via upload

---
+++ Source/wallCreate/GetData.cs
@@ -0,0 +1,23 @@
+﻿/*
+ * Created by SharpDevelop.
+ * User: Ashish Kamble
+ * Date: 23-04-2024
+ * Time: 18:01
+ * 
+ * To change this template use Tools | Options | Coding | Edit Standard Headers.
+ */
+using System;
+using Newtonsoft.Json.Linq;
+
+namespace wallCreate
+{
+	/// <summary>
+	/// Description of GetData.
+	/// </summary>
+	public class GetData
+	{
+		public static double getVertex(string vetexID, out JObject builderRoot){
+			return builderRoot["layers"][layerName]["vertices"][vetexID]["x"].ToObject<double>();
+		}
+	}
+}
